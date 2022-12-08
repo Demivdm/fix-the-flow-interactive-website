ac0ad8c67a5a17ac179418615a28b51fa498a231
Demivdm <demi.van.der.maarl@hva.nl>
2022-12-08 17:50:01 +0100

    popup
    popup gemaakt

---
+++ js.js
@@ -0,0 +1,8 @@
+console.log("why no work")
+
+document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(" .popup .sluitknop").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
